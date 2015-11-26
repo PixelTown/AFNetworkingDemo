6707f7ad5104c651111320f065e7e0ffc32470ff
YifangZhang <yifangzhang2009@gmail.com>
2015-11-25 22:24:44 -0600

    Initial Commit

---
+++ AFNetworkingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkingDemo
+//
+//  Created by Yifang Zhang on 11/25/15.
+//  Copyright © 2015 Yifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AFNetworkingDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AFNetworkingDemo
+//
+//  Created by Yifang Zhang on 11/25/15.
+//  Copyright © 2015 Yifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
